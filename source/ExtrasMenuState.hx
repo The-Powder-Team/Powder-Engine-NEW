fcb21f4fdde79ccf847b253f4cb059b9260eb306
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-09-18 19:53:20 -0400

    Update ExtrasMenuState.hx

--- source/ExtrasMenuState.hx
+++ source/ExtrasMenuState.hx
@@ -29,7 +29,7 @@ class ExtrasMenuState extends MusicBeatState
 	var menuItems:FlxTypedGroup<FlxSprite>;
 
 	#if !switch
-	var optionShit:Array<String> = ['story mode', 'freeplay', 'donate', 'options'];
+	var optionShit:Array<String> = ['credits', 'discord'];
 	#else
 	var optionShit:Array<String> = ['story mode', 'freeplay'];
 	#end
@@ -183,7 +183,7 @@ class ExtrasMenuState extends MusicBeatState
 
 			if (controls.BACK)
 			{
-				FlxG.switchState(new TitleState());
+				FlxG.switchState(new MainMenuState());
 			}
 
 			if (controls.ACCEPT)
