4da6a5933445596bd8a08bcc66f240f6ff030465
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-09-19 21:33:15 -0400

    it doesnt work waaa

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -12,12 +12,6 @@ class HealthIcon extends FlxSprite
 	public var isPlayer:Bool = false;
 	public var isOldIcon:Bool = false;
 
-	public var lossIndex:Int = -1;
-	public var neutralIndex:Int = 0;
-	public var winningIndex:Int = -1;
-
-	var charArray:Array<Int> = [];
-
 	/**
 	 * Used for FreeplayState! If you use it elsewhere, prob gonna annoying
 	 */
@@ -49,39 +43,15 @@ class HealthIcon extends FlxSprite
 		if (!OpenFlAssets.exists(Paths.image('icons/icon-' + char)))
 			char = 'face';
 
-		for (w in 0...Math.floor(width / 150))
-		{
-			charArray.push(w);
-		}
-		loadGraphic('icons/icon-' + char, true, 150, 150);
-		switch (charArray.length)
-		{
-			case 1:
+		loadGraphic(Paths.loadImage('icons/icon-' + char), true, 150, 150);
 
-			case 2:
-				neutralIndex = 0;
-				lossIndex = 1;
-			case 3:
-				lossIndex = 0;
-				neutralIndex = 1;
-				winningIndex = 2;
-		}
-		if (char == 'senpai' || char == 'spirit' || char.contains("pixel"))
-		{
-			antialiasing = false;
-		}
+		if (char.endsWith('-pixel') || char.startsWith('senpai') || char.startsWith('spirit'))
+			antialiasing = false
 		else
-		{
-			antialiasing = true;
-		}
-		animation.add('icon', charArray, 0, false);
-		animation.play('icon', true);
-		if (animation.curAnim != null)
-			animation.curAnim.curFrame = neutralIndex;
+			antialiasing = FlxG.save.data.antialiasing;
 
-		width = 150;
-		height = 150;
-		updateHitbox();
+		animation.add(char, [0, 1], 0, false, isPlayer);
+		animation.play(char);
 	}
 
 	override function update(elapsed:Float)
